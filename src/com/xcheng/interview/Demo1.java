97ca083504b329babb81b41d35ad20c8b43d1313
xucheng <xcheng_xc@sina.com>
2018-10-18 16:18:05 +0800

    //内部内测试 //要求：使用已知的变量，在控制台输出30，20，10。  // class Outer {  // public int num = 10;  // class Inner {  // public int num = 20;  // public void show() {  // int num = 30;  // System.out.println(?);  // System.out.println(??);  // System.out.println(???);  // }  // }  // }  // class InnerClassTest {  // public static void main(String[] args) {  // Outer.Inner oi = new Outer().new Inner();  // oi.show();  // }  // }

---
+++ src/com/xcheng/interview/Demo1.java
@@ -0,0 +1,42 @@
+package com.xcheng.interview;
+
+public class Demo1 {
+	// 要求：使用已知的变量，在控制台输出30，20，10。
+	// class Outer {
+	// public int num = 10;
+	// class Inner {
+	// public int num = 20;
+	// public void show() {
+	// int num = 30;
+	// System.out.println(?);
+	// System.out.println(??);
+	// System.out.println(???);
+	// }
+	// }
+	// }
+	// class InnerClassTest {
+	// public static void main(String[] args) {
+	// Outer.Inner oi = new Outer().new Inner();
+	// oi.show();
+	// }
+	// }
+	public static void main(String[] args) {
+		Outer.Inner oi = new Outer().new Inner();
+		oi.show();
+	}
+}
+
+class Outer {
+	public int num = 10;
+
+	class Inner {
+		public int num = 20;
+
+		public void show() {
+			int num = 30;
+			System.out.println(num);
+			System.out.println(this.num);
+			System.out.println(Outer.this.num);
+		}
+	}
+}
